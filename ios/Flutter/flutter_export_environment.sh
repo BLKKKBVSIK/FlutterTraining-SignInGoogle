358ebc456b969bbac12d30d4b543d52c110cf75a
Enzo CONTY <contact@enzoconty.dev>
2020-02-14 11:15:44 +0100

    It's actually genuine and working !

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/vincenzo/Documents/Projects/Flutter_Dev/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vincenzo/Documents/Projects/Flutter_Dev/sign_in_flutter-master"
+export "FLUTTER_TARGET=/Users/vincenzo/Documents/Projects/Flutter_Dev/sign_in_flutter-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/vincenzo/Documents/Projects/Flutter_Dev/SDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
